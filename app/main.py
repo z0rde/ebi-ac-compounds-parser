593c14bf70a436d34a919d85aa2e54101f2dd10c
z0rde <damedsama@gmail.com>
2022-07-04 01:04:09 +0400

    better save

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import time
 import random
+import requests
+import json
 
 # import psycopg2
 import rich
@@ -7,13 +9,14 @@ import sys
 
 from sqlalchemy import create_engine
 
+
 db_name = "compounds"
 db_user = "ebi-as"
 db_pass = "secrekt"
 db_host = "localhost"  # db
 db_port = "5432"
 
-
+compound_names = ["ADP", "ATP", "STI", "ZID", "DPM", "XP9", "18W", "29P"]
 # Connecto to the database
 db_string = "postgresql://{}:{}@{}:{}/{}".format(
     db_user, db_pass, db_host, db_port, db_name
@@ -49,13 +52,34 @@ def get_last_row():
 
 if __name__ == "__main__":
     print("Application started")
-    add_new_row(random.randint(1, 100000))
-    print("The last value insterted is: {}".format(get_last_row()))
+    # add_new_row(random.randint(1, 100000))
+    # print("The last value insterted is: {}".format(get_last_row()))
+
     # while True:
     # time.sleep(1)
     # add_new_row(random.randint(1, 100000))
     # print("The last value insterted is: {}".format(get_last_row()))
+
     if (len(sys.argv)) == 1:
-        print("USAGE: \n get <compound name>")
-        print("")
+        print("USAGE: \n\n get <compound name>")
+        print("or \n show <compound name>")
+        print("\nvalid compound names are:")
+        print(*compound_names, sep=", ")
+        quit()
+
+    if sys.argv[1] == "get":
+        if sys.argv[2].upper() not in compound_names:
+            print("\nInvalid compound name:", sys.argv[2].upper())
+            quit()
+        url = (
+            "https://www.ebi.ac.uk/pdbe/graph-api/compound/summary/"
+            + sys.argv[2].upper()
+        )
+        print(f"Requesting JSON from {url}")
+        text = requests.get(url)
+        text = text.text
+        data = json.loads(text)
+        print(type(data))
+        print(data)
+
     # print(sys.argv[1])
